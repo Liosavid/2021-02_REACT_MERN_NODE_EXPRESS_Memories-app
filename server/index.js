f0f534651d14d540860a687d9f8d45f1d2fbea39
Lionel Savidan <liosavid@gmail.com>
2021-02-09 23:08:12 +0000

    routing, controllers and models for posts created.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+// Set up starting path for all the routes inside posts.js
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -13,7 +18,6 @@ const CONNECTION_URL = 'mongodb+srv://liosavid:test1234@mytinerary.uopeb.mongodb
 const PORT = process.env.PORT || 5000;
 
 // Use Mongoose to connect to the database
-
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
         .catch((error)=> console.log(error.message));
